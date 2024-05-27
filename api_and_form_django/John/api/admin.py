5d83d5a3d19fe3a14a0f46c44b10bde0b016032a
NguyenVanHien17042003 <111997730+nguyenvanhien1704@users.noreply.github.com>
2024-05-28 02:51:06 +0700

    update

--- api_and_form_django/John/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Locaion, Item
-
-admin.site.register(Locaion)
-admin.site.register(Item)
--- demo_at_class/homepage/ulrs.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import api_news
-from .views import opennew
-urlpatterns = [
-
-    path('apinews/', api_news, name='api_news'),
-]
